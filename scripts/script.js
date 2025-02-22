c1c8096f1a26ed2af202e3099b2fe80bb9f3eadf
FromageEnCavale <julescoubetergues@icloud.com>
2025-02-22 20:11:27 +0100

    Permanent PWA remove

--- scripts/script.js
+++ scripts/script.js
@@ -127,25 +127,3 @@ userInput.addEventListener('keydown', (e) => {
     }
 
 });
-
-if ('serviceWorker' in navigator) {
-
-    window.addEventListener('load', () => {
-
-        navigator.serviceWorker.register('./sw.js')
-
-            .then(registration => {
-
-                console.log('Service Worker successfully registered :', registration);
-
-            })
-
-            .catch(error => {
-
-                console.error('Service Worker registration failed :', error);
-
-            });
-
-    });
-
-}
