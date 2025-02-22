ab2c9b178f743846946a2a573199ebecac284e4b
FromageEnCavale <julescoubetergues@icloud.com>
2025-02-22 23:30:22 +0100

    Add PWA app

--- scripts/script.js
+++ scripts/script.js
@@ -127,3 +127,25 @@ userInput.addEventListener('keydown', (e) => {
     }
 
 });
+
+if ('serviceWorker' in navigator) {
+
+    window.addEventListener('load', () => {
+
+        navigator.serviceWorker.register('./sw.js')
+
+            .then(registration => {
+
+                console.log('Service Worker successfully registered :', registration);
+
+            })
+
+            .catch(error => {
+
+                console.error('Service Worker registration failed :', error);
+
+            });
+
+    });
+
+}
